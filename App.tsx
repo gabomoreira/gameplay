b8d022dbf1d76e6dd15994c6e9bef161208001d9
Gabriel Moreira <gsm.br2003@gmail.com>
2023-03-31 11:46:52 -0300

    chore: add fonts and theme config

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import SignIn from './src/screens/SignIn';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter'
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  })
+
+  if(!fontsLoaded) return null
+
   return (
     <>
       <StatusBar backgroundColor='transparent' style='light' translucent />
--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,8 +1,26 @@
 export const theme = {
     colors: {
-        background: '#0D133D',
-        heading: '#DDE3F0',
         primary: '#E51C44',
-        line: '#991F36'
+        
+        secondary_100: '#0A1033',
+        secondary_90: '#0D133D',
+        secondary_80: '#0E1647',
+        secondary_70: '#1B2565',
+        secondary_50: '#243189',
+        secondary_40: '#1D2766',
+        secondary_30: '#495BCC',
+
+        background: '#0D133D',
+        overlay: 'rgba(0,0,0,0.7)',
+        hightlight: '#ABB1CC',
+        heading: '#DDE3F0',
+        line: '#991F36',
+        on: '#32BD50'
+    },
+    fonts: {
+        title_700: "Rajdhani_700Bold",
+        title_500: "Rajdhani_500Medium",
+        text_400: "Inter_400Regular",
+        text_500: "Inter_500Medium",
     }
 }
